5704dcbf1400483cba22469d4daac60998365055
Geert Fannes <geert.fannes@auro-3d.com>
2025-01-06 14:48:14 +0100

    Added multiple-cursors support

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,3 +7,10 @@ vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 vim.keymap.set({ "n", "v" }, "<leader>p", [["+p]])
 vim.keymap.set({ "n", "v" }, "<leader>P", [["+P]])
+
+vim.keymap.set({ "n" }, "-o", [[O<esc>]])
+vim.keymap.set({ "n" }, "-b", [[o{<cr>}<esc>O]])
+vim.keymap.set({ "n" }, "-m", [[oMSS();<esc>hi]])
+vim.keymap.set({ "n" }, "-n", [[oMSS_BEGIN(auro_ReturnCode_t);<cr>MSS_END();<esc>k$]])
+
+vim.keymap.set("n", "<C-v>", ":bd<CR>", { desc = "Close current buffer" })
