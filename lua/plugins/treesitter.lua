9d0c6f8fba5afa88cb70d3d4034c65ab8e1e8c57
Geert Fannes <geert.fannes@gmail.com>
2025-01-24 14:25:49 +0100

    Disabled treesitter indentation

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    config = {
+      -- Indentation with treesitter is not very consistent
+      indent = { enable = false },
+    },
+  },
+}
